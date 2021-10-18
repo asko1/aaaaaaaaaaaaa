f67f581b05e2d62cf14c912ac170e17253a9ea64
kaspar suursalu <kaspar.suursalu@tptlive.ee>
2021-10-18 11:20:13 +0300

    add posts and views

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function index(){
-        $array= ['apple', '<h1>orange</h1>', 'pear'];
-        return view('index', compact('array'));
+
+        $posts = Post::all();
+        return view('index', compact('posts'));
 
     }
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,18 +1,9 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    {!! $array[1] !!}
+@extends('layout')
+@section('title', 'Home Page')
+@section('content')
     <ul>
-        @foreach($array as $value)
-            <li>{{$value}}</li>
+        @foreach($posts as $post)
+            <li>{{$post->title}}</li>
         @endforeach
     </ul>
-</body>
-</html>
+@endsection
